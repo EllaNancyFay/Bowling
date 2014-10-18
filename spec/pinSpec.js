eba07fc4e7f69e748bdcf10aefa1f1f7fa56d4fd
EllaNancyFay <ellaschofield0@gmail.com>
2014-10-18 17:48:21 +0100

    Create pin object with hit property

---
+++ spec/pinSpec.js
@@ -0,0 +1,16 @@
+describe('Pin', function() {
+
+beforeEach(function() {
+	pin = new Pin;
+});
+
+it('should be upright once created', function() {
+	expect(pin.isUpright).toBe(true);
+});
+
+it('should change status when hit', function(){
+	pin.hit(pin);
+ 	expect(pin.isUpright).toBe(false);
+});
+
+}); 
