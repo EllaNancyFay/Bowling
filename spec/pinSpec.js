dd1ea87f2c15493012b4eb530c9e254287207580
EllaNancyFay <ellaschofield0@gmail.com>
2014-10-31 13:49:40 +0000

    Create frame score by adding the results of the two bowls

--- spec/pinSpec.js
+++ spec/pinSpec.js
@@ -18,34 +18,43 @@ it('should change status when hit', function(){
 describe('Frame', function() {
 
 	beforeEach(function() {
-	frame = new Frame
-	pin = new Pin
+		frame = new Frame
+		pin = new Pin
 	});
 
 	it('should hold pins', function() {
-	expect(frame.pins instanceof Array).toBe(true);
+		expect(frame.pins instanceof Array).toBe(true);
 	});
 
 	it('should hold 10 ten pins', function() {
-	expect(frame.pins.length).toEqual(10);
+		expect(frame.pins.length).toEqual(10);
 	});
 
 	it('should be able to knock down a number of pins', function() {
-	frame.bowl(0, 4)
-	expect(frame.pins.length).toEqual(6);
+		frame.bowl(0, 4)
+		expect(frame.pins.length).toEqual(6);
 	});
 
 	it('should add the result to the score', function() {
-	frame1 = new Frame;
-	frame1.bowl(0, 4)
-	expect(frame1.score).toEqual([4]);
+		frame1 = new Frame;
+		frame1.bowl(0, 4)
+		expect(frame1.score).toEqual([4]);
 	});
 
-	it('should only have two bowls', function() {
-	frame2 = new Frame;
-	frame2.bowl(0, 3);
-	frame2.bowl(0, 5);
-	expect(frame2.go()).toEqual('over');
+	it('should only have a maximum of two bowls', function() {
+		frame2 = new Frame;
+		frame2.bowl(0, 3);
+		frame2.bowl(0, 5);
+		expect(frame2.go()).toEqual('over');
 	});
+
+	it('should know the score of the frame', function() {
+		frame3 = new Frame;
+		frame3.bowl(0, 4);
+		frame3.bowl(0, 2);
+		expect(frame3.score.sum()).toEqual(6);
+	});
+
+
 
 });
--- src/pin.js
+++ src/pin.js
@@ -20,6 +20,11 @@ Frame.prototype.bowl = function(index, number) {
 };
 
 Frame.prototype.go = function() {
-	if(this.score.length == 2) {
+	if(this.score.length == 2 ) {
 	return 'over'}
+};
+
+Array.prototype.sum = function() { 
+	for (var i = 0, L = this.length, sum = 0; i < L; sum += this[i++]);
+	return sum; 
 };
