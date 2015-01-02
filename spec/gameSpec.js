11b29348d52ecae6e21422820e55331eeb603cd7
EllaNancyFay <ellaschofield0@gmail.com>
2015-01-02 16:39:25 +0000

    Now playing frames within the game

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,9 +1,11 @@
 describe('Game', function() {
 
   var game;
+  var frame;
 
   beforeEach(function() {
-    game = new Game
+    game = new Game();
+    frame = new Frame();
   });
 
   it('should have frames', function() {
@@ -11,31 +13,26 @@ describe('Game', function() {
   });
 
   it('should have a total of 10 frames', function() {
-  	game.setup();
   	expect(game.frames.length).toEqual(10);
   });
 
   it('should be able to play a frame', function() {
-    game.playFrame(2, 4);
-    expect(game.frames).toEqual([6]);
+    game.frames[0].bowl(3);
+    game.frames[0].bowl(4);
+    expect(game.frames[0].score).toEqual([3, 4]);
   });
 
-  it('should be able to play multiple frames', function() {
+  xit('should be able to play multiple frames', function() {
     game.playFrame(3, 3);
     game.playFrame(4, 6);
-    expect(game.frames).toEqual([6, 10]);
+    expect(game.frames).toEqual([[3, 3], [4, 6]]);
   });
 
-  it('should know the entire score of the frames', function() {
-    game.playFrame(1, 1);
-    game.playFrame(2, 2);
-    game.sumScore();
-    expect(game.entireScore).toEqual(6);
-  });
-
-  it('should know if a frame has a special score', function() {
-    game.playFrame(2, 8);
-    
+  xit('should save the bonus score when a strike is scored', function() {
+    game.playFrame(10, 0);
+    game.playFrame(1, 6);
+    game.applyBonus();
+    expect(game.bonuses).toEqual([1]);  
   });
 
 });
--- src/game.js
+++ src/game.js
@@ -1,22 +1,23 @@
 function Game() {
 	this.frames = [];
-  this.entireScore = 0;
-};
+  this.bonuses = [];
 
-Game.prototype.setup = function() {
-	for(var i = 0; i <= 9; i++) {
-	this.frames[i] = new Frame(i);
-	};
-};
+  for(var i = 0; i <= 9; i++) {
+   this.frames[i] = new Frame(i);
+  };
+};  
+
 
 Game.prototype.playFrame = function(bowlOne, bowlTwo) {
-  var frame = new Frame();
   frame.bowl(bowlOne);
   frame.bowl(bowlTwo);
   frame.tally();
-  this.frames.push(frame.totalScore);
 };
 
-Game.prototype.sumScore = function() {
-  this.entireScore = this.frames.sum();
-};
+// Game.prototype.applyBonus = function() {
+//   for (var i=0; i < 9; i++) {
+//     if (this.frames[i].sum() === 10) {
+//       this.bonuses.push(this.frames[i + 1][0])
+//     };
+//   };
+// };
